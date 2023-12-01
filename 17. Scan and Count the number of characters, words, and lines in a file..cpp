25023644f67388511e878d4fe6931a25d0fdc657
Sudarshandevagudi <112068014+Sudarshandevagudi@users.noreply.github.com>
2023-12-01 22:52:47 +0530

    Update 17. Scan and Count the number of characters, words, and lines in a file..cpp

--- 17. Scan and Count the number of characters, words, and lines in a file..cpp
+++ 17. Scan and Count the number of characters, words, and lines in a file..cpp
@@ -1,22 +1,14 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
-    char filename[100];
-    printf("Enter the filename: ");
-    scanf("%s", filename);
-
-    FILE *file = fopen(filename, "r");
-
-    if (file == NULL) {
-        printf("Error opening file %s\n", filename);
-        return 1;
-    }
-
     int characterCount = 0, wordCount = 0, lineCount = 0;
     int inWord = 0;
     char c;
 
-    while ((c = fgetc(file)) != EOF) {
+    printf("Enter text (Ctrl+D to end input):\n");
+
+    while ((c = getchar()) != EOF) {
         characterCount++;
 
         if (c == '\n') {
@@ -24,15 +16,13 @@ int main() {
             inWord = 0; // Reset inWord flag when a new line is encountered
         }
 
-        if (c == ' ' || c == '\n' || c == '\t') {
+        if (isspace(c)) {
             inWord = 0;
         } else if (inWord == 0) {
             inWord = 1;
             wordCount++;
         }
     }
-
-    fclose(file);
 
     // Check if the last word is not followed by a space or newline
     if (inWord == 1) {
